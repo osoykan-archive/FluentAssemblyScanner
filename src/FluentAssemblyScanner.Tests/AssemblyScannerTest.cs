97a3d188cf35b7f6558ccf3d054a5349dd160250
Ç. Çavuşoğlu <cavusoglucagatay@gmail.com>
2016-06-02 14:24:16 +0300

    initial unit tests

---
+++ src/FluentAssemblyScanner.Tests/AssemblyScannerTest.cs
@@ -0,0 +1,21 @@
+﻿#region License
+#endregion
+
+namespace FluentAssemblyScanner.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class AssemblyScannerTest
+    {
+        [Fact]
+        public void FromAssembly_IfAssemblyIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws(typeof(ArgumentNullException), () => AssemblyScanner.FromAssembly(null));
+        }
+    }
+}
