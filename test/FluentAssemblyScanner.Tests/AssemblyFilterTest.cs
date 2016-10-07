5955afb4946b6e2ccb35d70de1f36bf29e46931e
Oğuzhan Soykan <oguzhansoykan@gmail.com>
2016-10-07 18:26:29 +0300

    unit test are not supppted on xproj, until then codes are commented

--- test/FluentAssemblyScanner.Tests/AssemblyFilterTest.cs
+++ test/FluentAssemblyScanner.Tests/AssemblyFilterTest.cs
@@ -1,33 +1,33 @@
-﻿using System;
+﻿//using System;
 
-using Xunit;
+//using Xunit;
 
-namespace FluentAssemblyScanner.Tests
-{
-    public class AssemblyFilterTest
-    {
-        [Fact]
-        public void Ctor_IfDirectoryNameIsNull_ThrowsArgumentNullException()
-        {
-            Assert.Throws<ArgumentNullException>(() => new AssemblyFilter(null));
-        }
+//namespace FluentAssemblyScanner.Tests
+//{
+//    public class AssemblyFilterTest
+//    {
+//        [Fact]
+//        public void Ctor_IfDirectoryNameIsNull_ThrowsArgumentNullException()
+//        {
+//            Assert.Throws<ArgumentNullException>(() => new AssemblyFilter(null));
+//        }
 
-        [Fact]
-        public void FilterByAssembly_IfFilterIsNull_ThrowsArgumentNullException()
-        {
-            Assert.Throws<ArgumentNullException>(() => new AssemblyFilter(string.Empty).FilterByAssembly(null));
-        }
+//        [Fact]
+//        public void FilterByAssembly_IfFilterIsNull_ThrowsArgumentNullException()
+//        {
+//            Assert.Throws<ArgumentNullException>(() => new AssemblyFilter(string.Empty).FilterByAssembly(null));
+//        }
 
-        [Fact]
-        public void FilterByName_IfFilterIsNull_ThrowsArgumentNullException()
-        {
-            Assert.Throws<ArgumentNullException>(() => new AssemblyFilter(string.Empty).FilterByName(null));
-        }
+//        [Fact]
+//        public void FilterByName_IfFilterIsNull_ThrowsArgumentNullException()
+//        {
+//            Assert.Throws<ArgumentNullException>(() => new AssemblyFilter(string.Empty).FilterByName(null));
+//        }
 
-        [Fact]
-        public void WithKeyToken_IfPublicKeyTokenIsNull_ThrowsArgumentNullException()
-        {
-            Assert.Throws<ArgumentNullException>(() => new AssemblyFilter(string.Empty).WithKeyToken((byte[])null));
-        }
-    }
-}
+//        [Fact]
+//        public void WithKeyToken_IfPublicKeyTokenIsNull_ThrowsArgumentNullException()
+//        {
+//            Assert.Throws<ArgumentNullException>(() => new AssemblyFilter(string.Empty).WithKeyToken((byte[])null));
+//        }
+//    }
+//}
--- test/FluentAssemblyScanner.Tests/FluentAssemblyScannerTest.cs
+++ test/FluentAssemblyScanner.Tests/FluentAssemblyScannerTest.cs
@@ -1,84 +1,84 @@
-﻿using System;
+﻿//using System;
 
-using Xunit;
+//using Xunit;
 
-namespace FluentAssemblyScanner.Tests
-{
-    public class FluentAssemblyScannerTest
-    {
-        [Fact]
-        public void FromAssembly_IfAssemblyIsNull_ThrowsArgumentNullException()
-        {
-            Assert.Throws(typeof(ArgumentNullException), () => AssemblyScanner.FromAssembly(null));
-        }
+//namespace FluentAssemblyScanner.Tests
+//{
+//    public class FluentAssemblyScannerTest
+//    {
+//        [Fact]
+//        public void FromAssembly_IfAssemblyIsNull_ThrowsArgumentNullException()
+//        {
+//            Assert.Throws(typeof(ArgumentNullException), () => AssemblyScanner.FromAssembly(null));
+//        }
 
-        [Fact]
-        public void FromAssembly_ShouldReturnOneType()
-        {
-            var types = AssemblyScanner.FromThisAssembly()
-                                             .IncludeNonPublicTypes()
-                                             .BasedOn<IAnimal>()
-                                             .Filter()
-                                             .MethodHasAttribute<Voice>()
-                                             .Scan();
+//        [Fact]
+//        public void FromAssembly_ShouldReturnOneType()
+//        {
+//            var types = AssemblyScanner.FromThisAssembly()
+//                                             .IncludeNonPublicTypes()
+//                                             .BasedOn<IAnimal>()
+//                                             .Filter()
+//                                             .MethodHasAttribute<Voice>()
+//                                             .Scan();
 
-            Assert.Equal(types.Count, 1);
-        }
+//            Assert.Equal(types.Count, 1);
+//        }
 
-        [Fact]
-        public void FromAssemblyContaining_IfAssemblyIsNull_ThrowsArgumentNullException()
-        {
-            Assert.Throws(typeof(ArgumentNullException), () => AssemblyScanner.FromAssemblyContaining(null));
-        }
+//        [Fact]
+//        public void FromAssemblyContaining_IfAssemblyIsNull_ThrowsArgumentNullException()
+//        {
+//            Assert.Throws(typeof(ArgumentNullException), () => AssemblyScanner.FromAssemblyContaining(null));
+//        }
 
-        [Fact]
-        public void FromAssemblyInDirectory_IfFilterIsNull_ThrowsArgumentNullException()
-        {
-            Assert.Throws(typeof(ArgumentNullException), () => AssemblyScanner.FromAssemblyInDirectory(null));
-        }
-    }
+//        [Fact]
+//        public void FromAssemblyInDirectory_IfFilterIsNull_ThrowsArgumentNullException()
+//        {
+//            Assert.Throws(typeof(ArgumentNullException), () => AssemblyScanner.FromAssemblyInDirectory(null));
+//        }
+//    }
 
-    public interface IAnimal
-    {
-        IAnimal Walks();
-    }
+//    public interface IAnimal
+//    {
+//        IAnimal Walks();
+//    }
 
-    public interface IHuman : IAnimal
-    {
-        IHuman Runs();
-    }
+//    public interface IHuman : IAnimal
+//    {
+//        IHuman Runs();
+//    }
 
-    public interface IBird : IAnimal
-    {
-        IBird Flies();
-    }
+//    public interface IBird : IAnimal
+//    {
+//        IBird Flies();
+//    }
 
-    public class Human : IHuman
-    {
-        public IAnimal Walks()
-        {
-            return this;
-        }
+//    public class Human : IHuman
+//    {
+//        public IAnimal Walks()
+//        {
+//            return this;
+//        }
 
-        public IHuman Runs()
-        {
-            return this;
-        }
-    }
+//        public IHuman Runs()
+//        {
+//            return this;
+//        }
+//    }
 
-    public class Bird : IBird
-    {
-        [Voice]
-        public IAnimal Walks()
-        {
-            return this;
-        }
+//    public class Bird : IBird
+//    {
+//        [Voice]
+//        public IAnimal Walks()
+//        {
+//            return this;
+//        }
 
-        public IBird Flies()
-        {
-            return this;
-        }
-    }
+//        public IBird Flies()
+//        {
+//            return this;
+//        }
+//    }
 
-    public class Voice : Attribute { }
-}
+//    public class Voice : Attribute { }
+//}
